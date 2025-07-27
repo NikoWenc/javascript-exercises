e41d42a4949fbef34f1ee7fa18c15bee47b52b2f
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-07-28 00:28:37 +0800

    RepeatString exercise Done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeater) {
+    let newStr = '';
+    if (repeater < 0) {return "ERROR"};
+    for (let i = 0; i < repeater; i++){
+        newStr += str;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
