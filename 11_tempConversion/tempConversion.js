c8d33c9cba486df338af80014b893c20421d863e
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-07-28 19:13:23 +0800

    tempConversion Done

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = (temp - 32) * 5/9;
+  return (!Number.isInteger(result)) ? Number(result.toFixed(1)) : result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (temp * 9/5) + 32;
+  return (!Number.isInteger(result)) ? Number(result.toFixed(1)) : result;
 };
 
 // Do not edit below this line
