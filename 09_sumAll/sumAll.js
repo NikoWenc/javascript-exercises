6626593a731228a4759a4002e493fad05f147171
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-07-28 17:01:19 +0800

    refactor code and clean code for sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,17 +1,10 @@
 const sumAll = function(a, b) {
-    let total = a;
-    let min = a;
-    let max = b;
-    if (min > max) {
-        total = b;
-        min = b;
-        max = a; 
-    }else if (min < 0 || max < 0
-            || min - Math.floor(min) > 0 || max - Math.floor(max) > 0
-            || typeof(min) === 'string' || typeof(max) === 'string' 
-            || typeof(min) !== 'number' || typeof(max) !== 'number' ){
-        return "ERROR";
-    }
+    let [total, min, max] = [a, a, b];
+    // reverse the values if first number is higher
+    if (min > max) [total, a, b] = [b, b, a];
+    if (min < 0 || max < 0) return "ERROR";
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+
     for (min; min < max; min++){
         total += min + 1;
     }
