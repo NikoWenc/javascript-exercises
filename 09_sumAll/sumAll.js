72510642ae9dfde811b3d5ccbf3afa01a7c0d57e
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-07-28 13:02:26 +0800

    sumAll Done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let total = a;
+    let min = a;
+    let max = b;
+    if (min > max) {
+        total = b;
+        min = b;
+        max = a; 
+    }else if (min < 0 || max < 0
+            || min - Math.floor(min) > 0 || max - Math.floor(max) > 0
+            || typeof(min) === 'string' || typeof(max) === 'string' 
+            || typeof(min) !== 'number' || typeof(max) !== 'number' ){
+        return "ERROR";
+    }
+    for (min; min < max; min++){
+        total += min + 1;
+    }
+    return total;
 };
 
 // Do not edit below this line
