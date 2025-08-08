501137aa94f3efc2b40a2d956fa6449964db4b59
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-08-08 10:24:13 +0800

    Done 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+	for (let key in arr){
+    total += arr[key];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for (const key in arr) {
+    total *= arr[key];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    if (a === 0 || a === 1) {
+      return 1;
+    }
+    let result = 1;
+    for (let i = 2; i <= a; i++) {
+      result *= i;
+    }
+    return result;
 };
 
 // Do not edit below this line
