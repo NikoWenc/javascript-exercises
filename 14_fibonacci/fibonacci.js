05089c664fdab370736c4d5fc54f0b7373da9e86
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-08-08 23:31:54 +0800

    fibonacci Done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let a = 0;
+    let b = 1;
+    let fibo = [];
+    fibo.push(a);
+    fibo.push(b);
 
+    for (let i = 0; i < Number(index); i++){
+        fibo.push(fibo[a] + fibo[b]);
+        a++;
+        b++;
+    }
+
+    if (index < 0){return "OOPS"};
+
+    return fibo[Number(index)];
 };
 
 // Do not edit below this line
