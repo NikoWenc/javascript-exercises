b59966692adbf68deeb8020ff00b2e0b4bb72e26
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-08-08 23:03:19 +0800

    palindromes done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    const validLetters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    let mainStr = str.toLowerCase()
+                     .split('')
+                     .filter((string) => validLetters.includes(string))
+                     .join('');
+
+    let revStr = mainStr.split('').reverse().join('');
+
+    return mainStr === revStr;
 };
 
 // Do not edit below this line
